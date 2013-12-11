7ac07eec670611f81c6f7906447c1e41c1eb2db5
Kyle Roucis (Grimless) <grimless@kyleroucis.com>
2013-12-11 00:33:39 -0700

    Properly handle retain/release/dealloc for Nil/nil

--- ObjectVM/nil.c
+++ ObjectVM/nil.c
@@ -17,20 +17,47 @@
 
 #include <stdlib.h>
 
+#pragma mark Nil
+
+#pragma mark - Bound Methods
+
 static void nil_isNil_native(object* obj, clockwork_vm* vm)
 {
     vm_pushTrue(vm);
+    vm_return(vm);
 }
 
 static void nil_isTrue_native(object* obj, clockwork_vm* vm)
 {
     vm_pushFalse(vm);
+    vm_return(vm);
 }
 
 static void nil_isFalse_native(object* obj, clockwork_vm* vm)
 {
     vm_pushTrue(vm);
+    vm_return(vm);
 }
+
+static void nil_dealloc_native(object* obj, clockwork_vm* vm)
+{
+    vm_pushNil(vm);
+    vm_return(vm);
+}
+
+static void nil_retain_native(object* obj, clockwork_vm* vm)
+{
+    vm_pushSelf(vm);
+    vm_return(vm);
+}
+
+static void nil_release_native(object* obj, clockwork_vm* vm)
+{
+    vm_pushNil(vm);
+    vm_return(vm);
+}
+
+#pragma mark - Native Methods
 
 class* nil_class(struct clockwork_vm* vm)
 {
@@ -52,6 +79,17 @@ class* nil_class(struct clockwork_vm* vm)
         block* isFalseMethodNative = block_init_native(vm, NULL, &nil_isFalse_native);
         class_addClassMethod(nilClass, vm, "isFalse", isFalseMethodNative);
         class_addInstanceMethod(nilClass, vm, "isFalse", isFalseMethodNative);
+    }
+
+    {
+        block* deallocMethodNative = block_init_native(vm, NULL, &nil_dealloc_native);
+        class_addClassMethod(nilClass, vm, "dealloc", deallocMethodNative);
+        class_addInstanceMethod(nilClass, vm, "dealloc", deallocMethodNative);
+    }
+
+    {
+        class_addInstanceMethod(nilClass, vm, "retain", block_init_native(vm, NULL, &nil_retain_native));
+        class_addInstanceMethod(nilClass, vm, "release", block_init_native(vm, NULL, &nil_release_native));
     }
 
 #warning ADD forwardInvocation: TO DO NOTHING (IT IS SAFE TO MESSAGE nil/Nil).
